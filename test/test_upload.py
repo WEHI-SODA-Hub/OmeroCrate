603beb919648e8fd844c9056c4a356329e27f3d1
Michael Milton <ttmigueltt@gmail.com>
2025-03-07 16:17:55 +1100

    Add upload test

---
+++ test/test_upload.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from omerocrate.uploader import OmeroUploader, BlitzGateway
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+def test_upload_report(ca_imaging_1021: Path):
+    uploader = OmeroUploader(
+        conn = BlitzGateway(
+            username=os.environ["OMERO_USER"],
+            passwd=os.environ["OMERO_PASS"],
+            host=os.environ["OMERO_HOST"],
+            port=os.environ["OMERO_PORT"],
+            secure=True
+        ),
+        crate=ca_imaging_1021
+    )
+    uploader.execute()
