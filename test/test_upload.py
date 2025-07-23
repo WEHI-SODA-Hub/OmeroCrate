f2ca6ae55882c05a0334ec4d7a341421a6d451f4
Michael Milton <ttmigueltt@gmail.com>
2025-07-23 17:37:12 +1000

    Test for group owner

--- test/test_upload.py
+++ test/test_upload.py
@@ -16,6 +16,7 @@ async def test_upload_default(abstract_crate: Path, connection: BlitzGateway):
     dataset = await uploader.execute()
     assert dataset.name == "Abstract art"
     assert dataset.countChildren() == 1
+    assert dataset.getOwnerFullName() == "Abstract art"
     for image in dataset.listChildren():
         assert "Color Study" in image.name
     delete_dataset(dataset)
