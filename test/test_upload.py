1de0976310042830197f20c735c744b187e02e91
Michael Milton <ttmigueltt@gmail.com>
2025-07-25 13:58:54 +1000

    Skip taskqueue upload unless FLOWER_HOST is set

--- test/test_upload.py
+++ test/test_upload.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import pytest
-from omerocrate.uploader import OmeroUploader, ApiUploader
+from omerocrate.uploader import ApiUploader
 from omerocrate.taskqueue.upload import TaskqueueUploader
-from omero.gateway import BlitzGateway, ImageWrapper, DatasetWrapper
+from omero.gateway import BlitzGateway
+import os
 
 from omerocrate.utils import delete_dataset
 
@@ -20,6 +21,7 @@ async def test_upload_default(abstract_crate: Path, connection: BlitzGateway):
         assert "Color Study" in image.name
     delete_dataset(dataset)
 
+@pytest.mark.skipif(not os.environ.get("FLOWER_HOST"), reason="OMERO taskqueue not available")
 @pytest.mark.asyncio
 async def test_upload_queue(abstract_crate: Path, connection: BlitzGateway):
     load_dotenv()
