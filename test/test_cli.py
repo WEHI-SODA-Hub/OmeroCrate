1959386ece6945fb72741f4ebbc5ad1ac4bda5be
Michael Milton <ttmigueltt@gmail.com>
2025-07-28 17:09:21 +1000

    Try disabling pytest capture in CLI test

--- test/test_cli.py
+++ test/test_cli.py
@@ -6,8 +6,9 @@ from omerocrate.cli import app
 import pytest
 
 @pytest.mark.parametrize("uploader", ["omerocrate.ApiUploader", "omerocrate.TaskqueueUploader"])
-def test_cli(connection: BlitzGateway, abstract_crate: Path, uploader: str):
-    result = CliRunner(mix_stderr=False).invoke(app, [str(abstract_crate), "--uploader", uploader])
+def test_cli(connection: BlitzGateway, abstract_crate: Path, uploader: str, capsys: pytest.CaptureFixture[str]):
+    with capsys.disabled():
+        result = CliRunner(mix_stderr=False).invoke(app, [str(abstract_crate), "--uploader", uploader])
     # Can't query the dataset unless we are in the right group
     connection.setGroupNameForSession("Abstract art")
     # Parse the dataset ID from the output
